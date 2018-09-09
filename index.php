95d373345b4764cfe024f84280a80c62705148d6
Nematulloh <nematulloh95net@gmail.com>
2018-09-09 17:53:13 +0300

    create file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+    <h2>SORT FOR DATE</h2>
+    <form name="f1" action="results.php" method="get">
+          <select name="animal">
+              <option value="">select animal</option>
+              <option value="dog">dog</option>
+              <option value="cat">Cat</option>
+              <option value="tortoise">Tortoise</option>
+          </select>
+        <input type="submit" name="ok">
+    </form>
